77213a84a911a716b2b39266c13d7a6046ef86d7
ZainZ <zain-63@hotmail.com>
2019-05-26 02:59:58 -0400

    Reduce runtime and heapsize.

--- A0/a0_starter/CCServer.java
+++ A0/a0_starter/CCServer.java
@@ -1,24 +1,22 @@
 import java.io.*;
 import java.net.*;
-import java.nio.charset.*;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.lang.StringBuffer;
+
 import helper.Graph;
 import helper.Edge;
 
 class CCServer {
     public static void main(String args[]) throws Exception {
-	if (args.length != 1) {
-	    System.out.println("usage: java CCServer port");
-	    System.exit(-1);
-	}
-	int port = Integer.parseInt(args[0]);
+        if (args.length != 1) {
+            System.out.println("usage: java CCServer port");
+            System.exit(-1);
+        }
+        int port = Integer.parseInt(args[0]);
 
-	ServerSocket ssock = new ServerSocket(port);
-	System.out.println("listening on port " + port);
-	while(true) {
-	    try {
+        ServerSocket ssock = new ServerSocket(port);
+        System.out.println("listening on port " + port);
+        while (true) {
+            try {
 		/*
 		  YOUR CODE GOES HERE
 		  - accept connection from server socket
@@ -30,37 +28,34 @@ class CCServer {
 		  - the payload is a string
 		    (UTF-8, big-endian)
 		*/
-            Socket csock = ssock.accept();
-            System.out.println("Accepted connection from client");
-            DataInputStream din = new DataInputStream(csock.getInputStream());
-            int reqDataLen = din.readInt();
-            System.out.println("received request header, data payload has length " + reqDataLen);
-            byte[] bytes = new byte[reqDataLen-1]; // trailing whitespace
-            din.readFully(bytes);
-            String input = new String(bytes, StandardCharsets.UTF_8);
-            String[] splitted = input.split("\\W+");
-            Graph g = new Graph();
-            for (int i = 0; i < splitted.length; i+=2) {
-                int src = Integer.parseInt(splitted[i]);
-                int dest = Integer.parseInt(splitted[i+1]);
-                Edge e = new Edge(src,dest);
-                g.addEdge(e);
+                Socket csock = ssock.accept();
+                System.out.println("Accepted connection from client at: " + csock.getPort());
+                DataInputStream din = new DataInputStream(csock.getInputStream());
+                int reqDataLen = din.readInt();     // read the next 4 bytes as an int
+                System.out.println("received request header, data payload has length " + reqDataLen);
+                BufferedReader in = new BufferedReader(new InputStreamReader(din, "UTF-8"));  // read as UTF-8
+                Graph g = new Graph();
+                for (String line = in.readLine(); line != null; line = in.readLine()) {
+                    int src = Integer.parseInt(line.substring(0, line.indexOf(" ")));
+                    int dest = Integer.parseInt(line.substring(line.indexOf(" ") + 1, line.length()));
+                    g.addEdge(new Edge(src, dest));
+
+                    if (!in.ready())
+                        break;
+                }
+                HashMap<Integer, Integer> connectedComponents = g.connectedComponents();
+                StringBuilder output = new StringBuilder();
+                for (int vertex : connectedComponents.keySet()) {
+                    output.append(vertex + " " + connectedComponents.get(vertex) + "\n");
+                }
+                DataOutputStream dout = new DataOutputStream(csock.getOutputStream());
+                byte[] bytes = output.toString().getBytes("UTF-8");
+                dout.writeInt(bytes.length);
+                dout.write(bytes);
+                dout.flush();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            HashMap<Integer,Integer> connectedComponents = g.connectedComponents();
-            StringBuffer output = new StringBuffer();
-            for (int vertex: connectedComponents.keySet()) {
-                output.append(vertex + " " + connectedComponents.get(vertex) + "\n");
-            }
-            String ret = output.toString();
-            DataOutputStream dout = new DataOutputStream(csock.getOutputStream());
-            bytes = ret.getBytes("UTF-8");
-            dout.writeInt(bytes.length);
-            dout.write(bytes);
-            dout.flush();
-	    }
-        catch (Exception e) {
-		e.printStackTrace();
-	    }
-	}
+        }
     }
 }
