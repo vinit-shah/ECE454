66e41e2caf512d2ddf5b0d9823b6016442850d81
vinit-shah <vinitshaw@hotmail.com>
2019-06-30 19:56:04 -0400

    finished task3

--- A2/Task3.scala
+++ A2/Task3.scala
@@ -2,15 +2,17 @@ import org.apache.spark.{SparkContext, SparkConf}
 
 // please don't change the object name
 object Task3 {
+  def numRatings(movie:String):Array[(Long,Long)] = {
+      return movie.split(",")
+                  .zipWithIndex.filter(x => x._2 != 0)
+                  .map(x => if (x._1 == "") (x._2.toLong,0.toLong) else (x._2.toLong, 1.toLong))
+  }
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Task 3")
     val sc = new SparkContext(conf)
-
     val textFile = sc.textFile(args(0))
-
-    // modify this code
-    val output = textFile.map(x => x);
-    
+    val movies = textFile.flatMap(line => line.split("\n"))
+    val output = movies.map(numRatings).flatMap(_.toList).reduceByKey(_ + _)
     output.saveAsTextFile(args(1))
   }
 }
