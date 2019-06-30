d601f907319c940c84065bf6223ce29a567c1339
vinit-shah <vinitshaw@hotmail.com>
2019-06-30 19:23:14 -0400

    task 1 and 2 done in scala

--- A2/Task1.scala
+++ A2/Task1.scala
@@ -2,15 +2,22 @@ import org.apache.spark.{SparkContext, SparkConf}
 
 // please don't change the object name
 object Task1 {
+
+  def topRatings(movie:String):String = {
+      val ratings = movie.split(",").zipWithIndex.map(x => (x._2, x._1))
+      val title = ratings.filter(x => x._1 == 0)
+      val userRatings = ratings.filter(x=> x._1 > 0).map(x => if(x._2 == "") (x._1,0.toLong) else (x._1,x._2.toLong))
+      val max = userRatings.maxBy(_._2)._2
+      val maxes = userRatings.filter{x => x._2 == max}
+      val ret = title.map(x => x._2) ++ maxes.map(x => x._1.toString)
+      return ret.mkString(",")
+  }
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Task 1")
     val sc = new SparkContext(conf)
-
     val textFile = sc.textFile(args(0))
-
-    // modify this code
-    val output = textFile.map(x => x);
-    
+    val movies = textFile.flatMap(line => line.split("\n"))
+    val output = movies.map(topRatings)
     output.saveAsTextFile(args(1))
   }
 }
--- A2/Task2.scala
+++ A2/Task2.scala
@@ -2,15 +2,17 @@ import org.apache.spark.{SparkContext, SparkConf}
 
 // please don't change the object name
 object Task2 {
+  def numRatings(movie:String):(Long,Int) = {
+      return (0.toLong,movie.split(",").zipWithIndex
+                             .filter(x => x._2 != 0 && x._1 != "")
+                             .length)
+  }
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Task 2")
     val sc = new SparkContext(conf)
-
     val textFile = sc.textFile(args(0))
-
-    // modify this code
-    val output = textFile.map(x => x);
-    
+    val movies = textFile.flatMap(line => line.split("\n"))
+    val output = movies.map(numRatings).reduceByKey(_ + _).map(x => x._2)
     output.saveAsTextFile(args(1))
   }
 }
