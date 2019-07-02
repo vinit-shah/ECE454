ea569d39d611007c389e3221ec30349eb397f412
vinit-shah <vinitshaw@hotmail.com>
2019-07-01 21:35:54 -0400

    task 4 spark'

--- A2/Task4.scala
+++ A2/Task4.scala
@@ -2,15 +2,20 @@ import org.apache.spark.{SparkContext, SparkConf}
 
 // please don't change the object name
 object Task4 {
+  def computeSimilarities(pair:(String,String)):String = {
+      val movies = pair._1.split(",") zip pair._2.split(",")
+      val similarity = movies.filter(x => (x._1 == x._2) && (x._1 != "")).length
+      return List(movies(0)._1, movies(0)._2, similarity).mkString(",")
+  }
+
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Task 4")
     val sc = new SparkContext(conf)
 
     val textFile = sc.textFile(args(0))
-
+    val movies = sc.broadcast(textFile.flatMap(line => line.split("\n")).collect())
+    val pairs = movies.value.combinations(2).toList.map(x => if(x(0) < x(1)) (x(0), x(1)) else (x(1), x(0)))
     // modify this code
-    val output = textFile.map(x => x);
-    
+    val output = pairs.map(computeSimilarities)
     output.saveAsTextFile(args(1))
   }
-}
